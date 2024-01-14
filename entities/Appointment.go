31c480e50bb62175827621ac0b49e1cddd68ccd0
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-01-14 02:47:19 -0300

    feat: removing function that generating iD

--- entities/Appointment.go
+++ entities/Appointment.go
@@ -1,23 +1,11 @@
 package entities
 
-import (
-	"github.com/pborman/uuid"
-	"gorm.io/gorm"
-)
+import "gorm.io/gorm"
 
 type Appointment struct {
 	gorm.Model
-	ID string `json:"id"`
-
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Date        string `json:"date"`
 	Interval    int    `json:"interval"`
-}
-
-func NewAppointment() *Appointment {
-	appointment := Appointment{
-		ID: uuid.New(),
-	}
-	return &appointment
 }
