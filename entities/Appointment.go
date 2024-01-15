0c7767904bf382c5596d94579f36f9278a7ce397
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-01-15 15:26:54 -0300

    feat: Formated by JSON

--- entities/Appointment.go
+++ entities/Appointment.go
@@ -1,11 +1,13 @@
 package entities
 
-import "gorm.io/gorm"
+import "time"
 
 type Appointment struct {
-	gorm.Model
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Date        string `json:"date"`
-	Interval    int    `json:"interval"`
+	ID          int        `gorm:"primary_key" json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Date        string     `json:"date"`
+	Interval    int        `json:"interval"`
+	CreatedAt   *time.Time `db:"created_at" json:"created_at"`
+	UpdateAt    *time.Time `db:"update_at" json:"update_at"`
 }
