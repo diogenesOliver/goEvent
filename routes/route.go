832e645bf1f19c8bf898d7e03bb69cfd2e8ea3e8
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-01-18 11:48:57 -0300

    feat: Endpoint from create a new appointment created

--- routes/route.go
+++ routes/route.go
@@ -2,16 +2,13 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	controllers "github.com/goEvent/useCases"
 	usecases "github.com/goEvent/useCases"
 )
 
 func AppRoutes(r *gin.Engine) *gin.RouterGroup {
-	appointmentController := controllers.NewAppointment()
-
 	v1 := r.Group("/v1")
 	{
-		v1.POST("/events", appointmentController.CreateAppointments)
+		v1.POST("/create/appointment", usecases.CreateAppointments)
 		v1.GET("/appointments", usecases.GetAllAppointments)
 	}
 	return v1
