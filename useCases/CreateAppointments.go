49736345b40f521747a7b3efb3071642240fe9a6
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-01-24 14:11:28 -0300

    wip: creating CreateAppointment feature

--- useCases/CreateAppointments.go
+++ useCases/CreateAppointments.go
@@ -1,18 +1,39 @@
 package usecases
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/goEvent/configs"
 	"github.com/goEvent/entities"
+	"github.com/goEvent/repositories"
 )
 
-func (a *AppointmentController) CreateAppointments(ctx *gin.Context) {
-	appointment := entities.Appointment{}
-	if err := ctx.BindJSON(&appointment); err != nil {
-		return
+func CreateAppointments(ctx *gin.Context) {
+	request := CreateAppointmentRequest{}
+	ctx.BindJSON(&request)
+
+	appointment := entities.Appointment{
+		Title:       request.Title,
+		Description: request.Description,
+		Interval:    request.Interval,
+		Date:        request.Date,
 	}
 
-	a.appointments = append(a.appointments, appointment)
-	ctx.JSON(http.StatusOK, a.appointments)
+	repos := repositories.New(repositories.Options{
+		ReaderGorm: configs.GetReaderGorm(),
+		WriterGorm: configs.GetWriterGorm(),
+	})
+
+	fmt.Println(appointment)
+
+	err := repos.Appointment.Create(context.Background(), appointment)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.JSON(http.StatusOK, appointment)
 }
